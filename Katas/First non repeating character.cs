7e03ec435bfcb9743855b63cf3a1db8c9ebb0eaf
kwiniarski97 <kwiniarski97@gmail.com>
2018-04-28 19:23:34 +0200

    non repeating characters

---
+++ Katas/First non repeating character.cs
@@ -0,0 +1,13 @@
+﻿namespace Katas
+{
+    using System.Linq;
+
+    public class Kata
+    {
+        public static string FirstNonRepeatingLetter(string s)
+        {
+            var ret = s.GroupBy(z => char.ToLower(z)).FirstOrDefault(g => g.Count() == 1);
+            return (ret != null) ? ret.First().ToString() : string.Empty;
+        }
+    }
+}
---
+++ KatasTest/First non repeating character TEST.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KatasTest
+{
+    using Katas;
+
+    using NUnit.Framework;
+
+    class First_non_repeating_character_TEST
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("a", Kata.FirstNonRepeatingLetter("a"));
+            Assert.AreEqual("t", Kata.FirstNonRepeatingLetter("stress"));
+            Assert.AreEqual("e", Kata.FirstNonRepeatingLetter("moonmen"));
+        }
+    }
+}
