a3923d58b208c7e562ea517c9c8f588d0c4c475c
kwiniarski97 <kwiniarski97@gmail.com>
2018-04-29 13:16:05 +0200

    pascal to snake

---
+++ Katas/Convert PascalCase string into snake_case.cs
@@ -0,0 +1,29 @@
+﻿namespace Katas
+{
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Convert_PascalCase_string_into_snake_case
+    {
+        public static string ToUnderscore(int str)
+        {
+            return str.ToString();
+        }
+
+        public static string ToUnderscore(string str)
+        {
+            var output = new StringBuilder();
+            foreach (var ch in str.ToCharArray())
+            {
+                if (char.ToUpper(ch) == ch && output.Length > 0 && !char.IsDigit(ch))
+                {
+                    output.Append('_');
+                }
+
+                output.Append(char.ToLower(ch));
+            }
+
+            return output.ToString();
+        }
+    }
+}
---
+++ KatasTest/Convert PascalCase string into snake_caseTESTS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KatasTest
+{
+    using Katas;
+
+    using NUnit.Framework;
+
+    class Convert_PascalCase_string_into_snake_caseTESTS
+    {
+        [Test]
+        public void SampleTests()
+        {
+            Assert.AreEqual("test_controller", Convert_PascalCase_string_into_snake_case.ToUnderscore("TestController"));
+            Assert.AreEqual("this_is_beautiful_day", Convert_PascalCase_string_into_snake_case.ToUnderscore("ThisIsBeautifulDay"));
+            Assert.AreEqual("am7_days", Convert_PascalCase_string_into_snake_case.ToUnderscore("Am7Days"));
+            Assert.AreEqual("my3_code_is4_times_better", Convert_PascalCase_string_into_snake_case.ToUnderscore("My3CodeIs4TimesBetter"));
+            Assert.AreEqual("arbitrarily_sending_different_types_to_functions_makes_katas_cool", Convert_PascalCase_string_into_snake_case.ToUnderscore("ArbitrarilySendingDifferentTypesToFunctionsMakesKatasCool"));
+            Assert.AreEqual("1", Convert_PascalCase_string_into_snake_case.ToUnderscore(1), "Numbers should be turned to strings!");
+        }
+    }
+}
