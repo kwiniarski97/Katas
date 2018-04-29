5f52e0bdc55edbba3d139413ca70e4c50939deed
kwiniarski97 <kwiniarski97@gmail.com>
2018-04-29 13:53:05 +0200

    hex

--- Katas/RGB To Hex Conversion.cs
+++ Katas/RGB To Hex Conversion.cs
@@ -1,35 +1,15 @@
 ﻿namespace Katas
 {
+    using System;
+
     public class RGB_To_Hex_Conversion
     {
         public static string Rgb(int r, int g, int b)
         {
-            return FormatInput(r) + FormatInput(g) + FormatInput(b);
-        }
-
-        private static string FormatInput(int p0)
-        {
-            if (p0 > 255)
-            {
-                return 255.ToString("X");
-            }
-
-            if (p0 < 0)
-            {
-                return AddPrecedingZero(0.ToString("X"));
-            }
-
-            if (p0 < 16)
-            {
-                return AddPrecedingZero(p0.ToString("X"));
-            }
-
-            return p0.ToString("X");
-        }
-
-        private static string AddPrecedingZero(string x)
-        {
-            return "0" + x;
+            r = Math.Max(Math.Min(255, r), 0);
+            g = Math.Max(Math.Min(255, g), 0);
+            b = Math.Max(Math.Min(255, b), 0);
+            return string.Format("{0:X2}{1:X2}{2:X2}", r, g, b);
         }
     }
 }
