84e00e56c0fefcc7918b57d9121a0033b449abb8
kwiniarski97 <kwiniarski97@gmail.com>
2018-04-29 13:52:43 +0200

    rgb to hex

---
+++ Katas/RGB To Hex Conversion.cs
@@ -0,0 +1,35 @@
+﻿namespace Katas
+{
+    public class RGB_To_Hex_Conversion
+    {
+        public static string Rgb(int r, int g, int b)
+        {
+            return FormatInput(r) + FormatInput(g) + FormatInput(b);
+        }
+
+        private static string FormatInput(int p0)
+        {
+            if (p0 > 255)
+            {
+                return 255.ToString("X");
+            }
+
+            if (p0 < 0)
+            {
+                return AddPrecedingZero(0.ToString("X"));
+            }
+
+            if (p0 < 16)
+            {
+                return AddPrecedingZero(p0.ToString("X"));
+            }
+
+            return p0.ToString("X");
+        }
+
+        private static string AddPrecedingZero(string x)
+        {
+            return "0" + x;
+        }
+    }
+}
---
+++ KatasTest/RGB To Hex Conversion TESTS.cs
@@ -0,0 +1,21 @@
+﻿namespace KatasTest
+{
+    using Katas;
+
+    using NUnit.Framework;
+
+    public class RGB_To_Hex_Conversion_TESTS
+    {
+        [Test]
+        public void FixedTests()
+        {
+            Assert.AreEqual("FFFFFF", RGB_To_Hex_Conversion.Rgb(255, 255, 255));
+            Assert.AreEqual("FFFFFF", RGB_To_Hex_Conversion.Rgb(255, 255, 300));
+            Assert.AreEqual("000000", RGB_To_Hex_Conversion.Rgb(0, 0, 0));
+            Assert.AreEqual("9400D3", RGB_To_Hex_Conversion.Rgb(148, 0, 211));
+            Assert.AreEqual("9400D3", RGB_To_Hex_Conversion.Rgb(148, -20, 211), "Handle negative numbers.");
+            Assert.AreEqual("90C3D4", RGB_To_Hex_Conversion.Rgb(144, 195, 212));
+            Assert.AreEqual("D4350C", RGB_To_Hex_Conversion.Rgb(212, 53, 12), "Consider single hex digit numbers.");
+        }
+    }
+}
